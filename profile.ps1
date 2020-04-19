634c4977dc257c324dae4c9acde4da33616fd31f
Alexey Sosnin <sosnin.alexey@outlook.com>
2020-04-20 00:52:28 +0300

    setup missing dependencies in profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,18 +1,31 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-# Set-Theme Powerlevel10k-LeanWithDuration
 
 Get-ChildItem "$($env:HOME)\projects\GitHub\Ne4to\scripts\modules" -Filter *.psm1 -Recurse |
-    ForEach-Object { Import-Module $_.FullName }
+ForEach-Object { Import-Module $_.FullName }
 
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
-        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+# setup kubectl aliases
+$HasKubectlAliases = Test-Path "$Home\Documents\PowerShell\kubectl_aliases.ps1"
+if (! $HasKubectlAliases) {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shanoor/kubectl-aliases-powershell/master/kubectl_aliases.ps1" -OutFile "$Home\Documents\PowerShell\kubectl_aliases.ps1"
 }
 
 . $Home\Documents\PowerShell\kubectl_aliases.ps1
 
-Set-Theme tehrob2
+# setup oh-my-posh theme
+$themeName = "tehrob-ne4to"
+$theme = get-theme | Where-Object Name -eq $themeName
+if (! $theme) {
+    $version = [string](get-module oh-my-posh).Version
+    Copy-Item "$Home\projects\GitHub\Ne4to\scripts\external\$themeName.psm1" "$Home\Documents\PowerShell\Modules\oh-my-posh\$version\Themes\$themeName.psm1"
+}
+
+Set-Theme $themeName
