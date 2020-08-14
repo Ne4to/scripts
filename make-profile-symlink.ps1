adee73df32b687d149b30f18916962076ca450e5
Alexey Sosnin <sosnin.alexey@outlook.com>
2020-08-15 00:50:58 +0300

    add make-profile-symlink.ps1

---
+++ make-profile-symlink.ps1
@@ -0,0 +1,12 @@
+# make a backup
+if (Test-Path $profile) {
+    $PathSuffix = Get-Date -Format ".yyyy-MM-dd-HHmmss.bak"
+    $TargetPath = $profile + $PathSuffix
+    Copy-Item $profile $TargetPath
+}
+
+$GitHubProfilePath = Join-Path -Path $HOME -ChildPath "projects\GitHub\Ne4to\scripts\profile.ps1"
+if (Test-Path $GitHubProfilePath) {
+    Remove-Item $profile -Force
+    New-Item $profile -ItemType SymbolicLink -Value $GitHubProfilePath
+}
